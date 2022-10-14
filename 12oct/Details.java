f82dbca4d55b0c9d1af9a8dc2afed19158db0bef
Avinash764489 <115772419+Avinash764489@users.noreply.github.com>
2022-10-14 11:12:13 +0530

    Add files via upload

---
+++ 12oct/Details.java
@@ -0,0 +1,8 @@
+class Details{
+	public static void main(String arg[]){
+		System.out.println("Name:- "+ arg[0] + " "+ arg[1]);
+		System.out.println("Age:- "+ arg[2]);
+		System.out.println("Course:- "+ arg[3]);
+		System.out.println("Grade:- "+ arg[4]);
+	}
+}
---
+++ 12oct/NameCMD.java
@@ -0,0 +1,5 @@
+class NameCMD{
+	public static void main(String arg[]){
+		System.out.println("Welcome "+arg[0] +" "+arg[1]);
+	}
+}
