cbf946c9665ff3777c292615b183ad57ec6c37b1
akk76 <akk76@Avinash>
2022-11-08 11:54:21 +0530

    Multiple inheritance and method overriding

--- Core_java/nov_08/methodOverriding/Parent2.java
+++ Core_java/nov_08/methodOverriding/Parent2.java
@@ -1,11 +1,11 @@
 package methodOverriding;
 
-//declare class
+//declare class and inherit Parent class 
 public class Parent2 extends Parent{
 
 	//declare method
 	public void display() {
-		super.display();
+		super.display();  //call parent class method
 		System.out.println("Parent class 2");
 	}
 	//end of method
--- Core_java/nov_08/methodOverriding/Parent3.java
+++ Core_java/nov_08/methodOverriding/Parent3.java
@@ -1,11 +1,11 @@
 package methodOverriding;
 
-//declare class
+//declare class and inherit Parent2 class
 public class Parent3 extends Parent2 {
 
 	//declare method
 	public void display() {
-		super.display();
+		super.display();  //call parent class method
 		System.out.println("Parent class 3");
 	}
 	//end of method
