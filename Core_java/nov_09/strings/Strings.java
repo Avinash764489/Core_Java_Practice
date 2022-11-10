7ac8ff2103c155e65b78e2444a5836f13005a03d
akk76 <akk76@Avinash>
2022-11-10 14:06:28 +0530

    nov_09

---
+++ Core_java/nov_09/strings/Strings.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class Strings {
+	
+	public static void main(String arg[]) {
+		
+		int i = 8;
+		i++;
+		System.out.println(i);  //mutable
+		String str = "Anudeep";
+		str = str.concat(" Foundation");
+		System.out.println(str);  //immutable
+	}
+}
